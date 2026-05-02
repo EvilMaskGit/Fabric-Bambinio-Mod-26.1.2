4fa39fc4ddb2aaa1dd7b36c364afafa0b302f03d
EvilMaskGit <filzujevs007@gmail.com>
2026-05-02 20:13:55 +0100

    Add files via upload

---
+++ src/main/java/net/bambinio/bambiniomod/BambinioMod.java
@@ -0,0 +1,16 @@
+package net.bambinio.bambiniomod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BambinioMod implements ModInitializer {
+	public static final String MOD_ID = "bambiniomod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/bambinio/bambiniomod/BambinioModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bambinio.bambiniomod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BambinioModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
